61cffafa8e35c0c4b0d8712e62a7cb7a54de8143
k.xiao <121974139+kevinxiao27@users.noreply.github.com>
2024-10-16 00:59:11 -0700

    Feat: create front-page

--- frontend/src/app/geocaches/page.tsx
+++ frontend/src/app/geocaches/page.tsx
@@ -1,12 +1,13 @@
+import Auth from "@/components/Auth/Auth";
 import { MapView } from "@/components/MapComponent/Map";
 import fetchBackend from "@/utils/fetchData";
 
 export default async function Page() {
   const { data: geocaches, count, error } = await fetchBackend({ method: "GET" });
   return (
-    <>
-      <div className="flex flex-row items-center justify-center text-white p-3 font-700 text-3xl">GeoPlots</div>
+    <Auth>
+      <div className='flex flex-row items-center justify-center text-white p-3 font-700 text-3xl'>GeoPlots</div>
       <MapView geocaches={geocaches} count={count} error={error} />
-    </>
+    </Auth>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,60 @@
-import { MapView } from "@/components/MapComponent/Map";
-import fetchBackend from "@/utils/fetchData";
-import Auth from "@/components/Auth/Auth";
+"use client";
+import Demo from "../../public/demo.png";
+import { LocateFixed } from "lucide-react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
-export default async function Page() {
-  const { data: geocaches, count, error } = await fetchBackend({ method: "GET", collection: "geocache" });
+export default function Page() {
   return (
     <>
-      <Auth>
-        <div className='flex flex-row items-center justify-center text-white p-3 font-700 text-3xl'>GeoPlots</div>
-        <MapView geocaches={geocaches} count={count} error={error} />
-      </Auth>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-black'>
+        <div className='text-5xl md:text-6xl text-white font-extrabold flex items-center gap-x-3'>
+          <div>GEOPLOTS</div>
+          <LocateFixed size={50} />
+        </div>
+
+        <div className='relative mt-8 w-[80vw]'>
+          <Image src={Demo} className='object-cover rounded-lg' alt='demo image' />
+
+          <div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-60 backdrop-blur-md rounded-lg p-8 flex flex-col justify-center items-center gap-y-4'>
+            <motion.div
+              className='text-center font-normal text-2xl md:text-3xl text-white'
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+            >
+              Your personal geodata journaling app:
+            </motion.div>
+            <motion.div
+              className='text-center font-normal text-2xl md:text-3xl text-white'
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              What you need
+            </motion.div>
+            <motion.div
+              className='text-center font-normal text-2xl md:text-3xl text-white'
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              Whenever you need it
+            </motion.div>
+
+            <div className='flex gap-x-4 mt-6'>
+              <Link href='/sign-in'>
+                <Button className='w-24 h-12 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition'>Login</Button>
+              </Link>
+              <Link href='/sign-up'>
+                <Button className='w-24 h-12 bg-red-600 text-white rounded-lg hover:bg-red-500 transition'>Sign Up</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
